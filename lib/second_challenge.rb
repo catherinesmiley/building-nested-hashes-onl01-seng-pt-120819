2f0b3f46c4f6905e04d10bd7e249c4d0aa66e13c
Catherine Smiley <catherinesmiley@gmail.com>
2020-01-05 22:51:44 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -17,3 +17,32 @@
 
   
 # end
+
+
+
+
+
+
+
+
+
+
+
+
+
+def second_challenge
+  epic_tragedy = {
+    :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+    },
+    :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+    }
+  }
+end
